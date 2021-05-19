387b9997c2d23d8f5e15464ba3798dfed7f25949
novakvova <novakvova@gmail.com>
2021-05-19 19:22:03 +0300

    Add redux base app

--- WebShopReact/my-app/src/index.js
+++ WebShopReact/my-app/src/index.js
@@ -1,14 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function counter(state = { name: '' }, action) {
+  return state;
+}
+
+const store = createStore(counter,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
