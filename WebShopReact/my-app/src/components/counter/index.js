d5f2b04e4fea61c31d419861c18a7f31633974bf
novakvova <novakvova@gmail.com>
2021-06-16 18:24:39 +0300

    Add swagger begin

--- WebShopReact/my-app/src/components/counter/index.js
+++ WebShopReact/my-app/src/components/counter/index.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as types from './types';
+import {push} from 'connected-react-router';
 
 export class CounterPage extends Component {
 
     buttonClick = () => {
         console.log("----дякую----", "click btn");
         this.props.dispatch({type: types.COUNTER_INCREMENT});
+
         //this.setState()
     }
 
     render() {
         console.log("-----props-----", this.props);
         const {counter} = this.props; //деструктуризація
+        if(counter == 30)
+        {
+            this.props.dispatch(push("/about"));
+        }
         return (
             <div>
                 <h1>Counter</h1>
